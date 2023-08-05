4c24d0986c9ebe2cf3044e1fa6ad8dc417cb4f20
tunahan özkan <mhnds_tunahan@hotmail.com>
2023-08-06 00:59:34 +0300

    Add project files.

---
+++ DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_IdentityRole.DAL
+{
+    public class Context:IdentityDbContext<AppUser,AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-GQ26LQR\\SQLEXPRESS;database= CasgemDbRole;integrated security = true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DAL/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Casgem_IdentityRole.DAL
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public int ProductName { get; set; }
+    }
+}
